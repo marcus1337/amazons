b11e6492236b8ce63a35b0e6fd6163432d2d1b75
marcus1337 <blurgas@hotmail.com>
2023-02-10 17:22:42 +0100

    api

---
+++ src/board/point.rs
@@ -0,0 +1,38 @@
+use std::ops::{Add, AddAssign, Sub};
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Point {
+    pub col: i32,
+    pub row: i32,
+}
+
+impl Point {
+    pub fn new(col: i32, row: i32) -> Self {
+        Self { col: col, row: row }
+    }
+
+    pub fn in_bounds(&self) -> bool {
+        self.col >= 0 && self.col < 6 && self.row >= 0 && self.row < 6
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+    fn add(self, other: Point) -> Point {
+        Point::new(self.col + other.col, self.row + other.row)
+    }
+}
+impl Sub for Point {
+    type Output = Point;
+    fn sub(self, other: Point) -> Point {
+        Point::new(self.col - other.col, self.row - other.row)
+    }
+}
+
+impl AddAssign for Point {
+    fn add_assign(&mut self, other: Self) {
+        self.col += other.col;
+        self.row += other.row;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 
 extern crate libc;
+pub mod board;
+
+use board::point::Point;
 
 #[repr(C)]
 pub struct Amazons {
@@ -12,6 +15,88 @@ impl Amazons{
     pub extern "C" fn amazons_make() -> Self {
         Self {
         }
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_reset(&mut self) {
+        *self = Amazons::amazons_make();
+    }
+
+    #[no_mangle]
+    pub extern "C" fn amazons_is_game_over(&mut self) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_is_one_winner(&mut self) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_is_two_winner(&mut self) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_can_undo(&self) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_can_redo(&self) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_undo(&mut self) {
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_redo(&mut self) {
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_is_started(&self) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_is_white_turn(&self) -> bool {
+        false
+    }
+
+    #[no_mangle]
+    pub extern "C" fn amazons_get_last_move_or_drop(&self) -> [Point;2] {
+        [Point::new(0,0),Point::new(0,0)]
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_get_num_stored_actions(&self) -> i32 {
+        0
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_get_stored_move_or_drop(&self, action_number: i32) -> [Point;2] {
+        [Point::new(0,0),Point::new(0,0)]
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_has_brick(&self, point: Point) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_has_player_brick(&self, point: Point) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_has_player_one_brick(&self, point: Point) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_can_move(&self, from: Point, to: Point) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_can_drop(&self, point: Point) -> bool {
+        false
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_move(&self, from: Point, to: Point) {
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_drop(&self, point: Point) {
+    }
+    #[no_mangle]
+    pub extern "C" fn amazons_get_ai_move_or_drop(&self) -> [Point; 2] {
+        [Point::new(0,0),Point::new(0,0)]
     }
 
 }
